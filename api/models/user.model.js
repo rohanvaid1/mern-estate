1dfcf9945fb50107185d88f8ef95b20e70ea1c2e
Git Hub <rohanit0002@gmail.com>
2025-03-31 12:21:52 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,23 +1,45 @@
-import { string } from "i/lib/util";
+// import { String } from "i/lib/util";
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     username: {
+//        type : String,
+//        required: true, 
+//        unique: true,
+//     },
+//     email: {
+//         type : String,
+//         required: true, 
+//         unique: true,
+//      },
+//      password: {
+//         type : String,
+//         required: true, 
+//      }
+// }, {timestamps : true});
+
+// const User = mongoose.model('User' ,userSchema) 
+
+// export default User;
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: {
-       type : string,
-       require: true, 
+       type : String,  // This will work with Mongoose natively
+       required: true, 
        unique: true,
     },
     email: {
-        type : string,
-        require: true, 
+        type : String,  // Again, this works without importing anything
+        required: true, 
         unique: true,
      },
      password: {
-        type : string,
-        require: true, 
+        type : String,  // Same here, no need for an import
+        required: true, 
      }
 }, {timestamps : true});
 
-const User = mongoose.model('User' ,userSchema) 
+const User = mongoose.model('User' ,userSchema);
 
 export default User;
